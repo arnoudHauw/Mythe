016c4b84a12559322dd4d08cb31cff49a8c78f0d
arnoudHauw <fiets_arnoud@live.nl>
2015-02-16 10:56:03 +0100

    mapStructure

---
+++ Assets/Scripts/ScriptsArnoud/BackGroundMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.down * (2 * Time.deltaTime));
+
+
+	}
+}
