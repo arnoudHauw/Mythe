32fcfd446f3f5e7a93a41bc95a6078e1a37a50be
Collix <15986@ma-web.nl>
2015-02-19 12:05:22 +0100

    Torch health, camera Angle and lose Object movement.
    Some components for the game. ^^

---
+++ Assets/Scripts/ScriptsCollin/BarrelScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelScript : MonoBehaviour
+{
+
+    private GameObject thePlayer;
+    private TorchScript torchScr;
+    void Start()
+    {
+        thePlayer = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        torchScr = thePlayer.GetComponent<TorchScript>();
+        torchScr.torchHealth = 10;
+    }
+}
---
+++ Assets/Scripts/ScriptsCollin/SlidePreview.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidePreview : MonoBehaviour
+{
+    private int turnDir = 1;
+    private Vector3 direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.rotation.z > 0.03)
+        {
+            turnDir = -1;
+        }
+        else if(this.transform.rotation.z < -0.03)
+        {
+            turnDir = 1;
+        }
+
+        direction = new Vector3(0, 0, this.transform.rotation.y + (Time.deltaTime / 2 * turnDir));
+        this.transform.Rotate(direction);
+    }
+}
