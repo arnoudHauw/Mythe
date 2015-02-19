d29d0807c2cccd9783fc5262381a7171d7a75120
bradakan <rocky-tempelaars@live.nl>
2015-02-19 19:07:38 +0100

    eerste versie van controls

---
+++ Assets/Scripts/ScriptsRocky/Gameplay/MovementController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour
+{
+
+    Touch touch1;
+    PlayerMovement playerMovement;
+    float cooldown = 0;
+    float cooldowndelay = 1;
+    void Start() 
+    {
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    public void click()
+    {
+
+        //stores the new touch into a variable so you can check the position in the update
+       touch1 = Input.touches[Input.touchCount - 1];
+       Debug.Log("click");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks  if there are any touches
+        if (Input.touchCount > 0)
+        {
+            //loop trough each touch to look for the touch we stored in click()  
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.fingerId == touch1.fingerId)
+                {
+                    //Debug.Log("result if thisx - mousex" + (Input.mousePosition.x - transform.position.x) as string);
+                    playerMovement.horizotalMovement((Input.mousePosition.x - transform.position.x));
+                }
+                if (touch.fingerId != touch1.fingerId && Time.time > cooldown)
+                {
+                    cooldown = Time.time + cooldowndelay;
+                    playerMovement.jump();
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptsRocky/Gameplay/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float speedcap = 25;
+    float jumpForce = 200;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void horizotalMovement(float speed) 
+    {
+
+        
+        if(speed > 50)
+        {
+            speed = speedcap;
+        }
+        if(speed < -50)
+        {
+            speed = -speedcap;
+        }
+        Debug.Log("speed = " + speed as string);
+        rigidbody2D.AddForce(new Vector2(speed/2, 0));
+       // transform.Translate(speed * Time.deltaTime,0,0);
+    }
+    public void jump() 
+    {
+        Debug.Log("jump");
+        rigidbody2D.AddForce(new Vector2(0,jumpForce));
+    }
+}
