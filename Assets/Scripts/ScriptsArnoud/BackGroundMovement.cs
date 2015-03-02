7a79ff51b86940882c2deb3fd8a83c398f482b23
arnoudHauw <fiets_arnoud@live.nl>
2015-03-02 09:19:29 +0100

    First prototype

--- Assets/Scripts/ScriptsArnoud/BackGroundMovement.cs
+++ Assets/Scripts/ScriptsArnoud/BackGroundMovement.cs
@@ -3,13 +3,7 @@ using System.Collections;
 
 public class BackGroundMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.down * (2 * Time.deltaTime));
+        transform.Translate(Vector3.down * (-2 * Time.deltaTime));
 	}
 }
