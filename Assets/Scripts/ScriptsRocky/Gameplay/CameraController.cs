82b50210aec2d9ec8c76889c39211c892747716a
bradakan <rocky-tempelaars@live.nl>
2015-03-09 12:06:16 +0100

    commit before pull

--- Assets/Scripts/ScriptsRocky/Gameplay/CameraController.cs
+++ Assets/Scripts/ScriptsRocky/Gameplay/CameraController.cs
@@ -3,10 +3,29 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-
+    private float turnDir = 1;
+    private Vector3 direction;
+    private float turnSpeed = 1.5f;
     // Update is called once per frame
     void Update()
     {
         Debug.Log(Input.acceleration.x);
+
+        if (Input.acceleration.x > 0.15f)
+        {
+            turnDir = turnSpeed;
+        }
+        else if (Input.acceleration.x < -0.15f)
+        {
+            turnDir = -turnSpeed;
+        }
+        else 
+        {
+            turnDir = 0;
+        }
+
+        direction = new Vector3(0, 0, this.transform.rotation.y + (Time.deltaTime / 2 * turnDir));
+        this.transform.Rotate(direction);
+
     }
 }
---
+++ Assets/Scripts/ScriptsRocky/UI/Score.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Text score;
+    int _score = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeScore(1);
+    }
+
+    void changeScore(int change)
+    {
+        _score += change;
+        score.text = "Score " + _score.ToString();
+    }
+}
