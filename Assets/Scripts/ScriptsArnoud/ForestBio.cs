fedb206860ec851c62857e1a85bc003080d93f39
arnoudHauw <fiets_arnoud@live.nl>
2015-03-30 12:04:05 +0200

    Chunk Update 3

---
+++ Assets/Scripts/ScriptsArnoud/ForestBio.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForestBio : MonoBehaviour {
+
+    public BackgroundType _type;
+    public GameObject SpawnPoint;
+
+    void Update()
+    {
+        if (this.gameObject.transform.position.y >= 0 && this.gameObject.transform.position.y <= 1)
+        {
+            SpawnPoint.GetComponent<BackGroundSpawn>().backgroundType = BackgroundType.Forest;
+        }
+    }
+}
